3dca559d435132becf50ee8a6b0c17b273f17765
WumpFroot <itswumpa@gmail.com>
2025-02-19 00:42:14 +0200

    Added GET request for a single note

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ app.get("/", (req, res) => {
 	res.send("<h1>Hello <i>express</i></h1>");
 });
 
+// GET request for all notes
 app.get("/api/notes", (req, res) => {
 	res.json(notes);
+});
+
+// GET request for a single note
+app.get("/api/notes/:id", (req, res) => {
+	const id = req.params.id;
+	const note = notes.find((note) => note.id === id);
+
+	if (!note) {
+		return res.status(404).json({ ERROR: `404: Note ${id} does not exist...` });
+	}
+	res.json(note);
 });
 
 const PORT = 8000;
