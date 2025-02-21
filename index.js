d0a21a07a8dc8efac845fa84a0ddd14149e898bd
WumpFroot <itswumpa@gmail.com>
2025-02-22 00:53:39 +0200

    Added contacts, step 1 done

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 import express from "express";
-import notes from "./notes.js";
+import persons from "./persons.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello <i>express</i></h1>");
 });
 
-// GET request for all notes
-app.get("/api/notes", (req, res) => {
-	const important = req.query.important;
-	console.log(important);
-
-	if (important === "true") {
-		const filNotes = notes.filter((note) => note.important === true);
-		res.json(filNotes);
-	} else if (important === "false") {
-		const filNotes = notes.filter((note) => note.important === false);
-		res.json(filNotes);
-	} else res.json(notes);
-});
-
-// GET request for a single note
-app.get("/api/notes/:id", (req, res) => {
-	const id = req.params.id;
-	const note = notes.find((note) => note.id === id);
-
-	if (!note) {
-		return res.status(404).json({ ERROR: `404: Note ${id} does not exist...` });
-	}
-	res.json(note);
+// GET request for all contacts
+app.get("/api/persons", (req, res) => {
+	res.json(persons);
 });
 
 const PORT = 8000;
