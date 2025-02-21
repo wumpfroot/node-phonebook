8411e5d393b52c49236a1c0e4dee768717e4aa9c
WumpFroot <itswumpa@gmail.com>
2025-02-22 01:15:43 +0200

    Step 3 done: request to fetch a single person

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
 
+// GET request for a single contact
+app.get("/api/persons/:id", (req, res) => {
+	const id = req.params.id;
+	const person = persons.find((person) => person.id === id);
+
+	if (!person) {
+		return res.status(404).send(`Person with the id of ${id} not found`);
+	}
+
+	res.json(person);
+});
+
 const PORT = 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
