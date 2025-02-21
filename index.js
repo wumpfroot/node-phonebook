cc2d85fed502b8dbc14edd6c88a509e0c02c9efd
WumpFroot <itswumpa@gmail.com>
2025-02-22 01:06:25 +0200

    Step 2 done: added a request for an info page

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
 	res.send("<h1>Hello <i>express</i></h1>");
 });
 
+// GET request for an info page
+app.get("/info", (req, res) => {
+	res.send(`Phonebook has info for ${persons.length} people\n${new Date()}`);
+});
+
 // GET request for all contacts
 app.get("/api/persons", (req, res) => {
 	res.json(persons);
