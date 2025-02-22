5b6bfb6d5bbe5ebb6e3c693af8c0f5dd68383881
WumpFroot <itswumpa@gmail.com>
2025-02-22 02:06:50 +0200

    Step 5 done: POST request to add a new person

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import persons from "./persons.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello <i>express</i></h1>");
@@ -27,7 +28,7 @@ app.get("/api/persons/:id", (req, res) => {
 		return res.status(404).send(`Person with the id of ${id} not found`);
 	}
 
-	res.json(person);
+	return res.json(person);
 });
 
 // DELETE request for a single contact
@@ -35,10 +36,27 @@ app.delete("/api/persons/:id", (req, res) => {
 	const id = req.params.id;
 	const index = persons.findIndex((person) => person.id === id);
 
-	if (index === -1) res.status(404).send(`Person with the id of ${id} not found`);
+	if (index === -1) return res.status(404).send(`Person with the id of ${id} not found`);
 
 	persons.splice(index, 1);
-	res.json(persons);
+	return res.json(persons);
+});
+
+// POST request to add a person
+app.post("/api/persons", (req, res) => {
+	const { name, number } = req.body;
+	const id = Math.floor(Math.random() * 9999) + (persons.length + 1);
+
+	const person = {
+		id: id.toString(),
+		name: name,
+		number: number,
+	};
+
+	if (!name || !number) return res.status(400).send("Fill all the fields");
+
+	persons.push(person);
+	return res.status(201).json({ "new person added": persons });
 });
 
 const PORT = 8000;
